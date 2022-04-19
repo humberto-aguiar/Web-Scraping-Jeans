1ac40c6d128bb6769d045a8be3579dbd6ae8fe96
humberto-aguiar <hbasf13@gmail.com>
2022-04-19 13:04:59 -0300

    fix deploy.sh file

---
+++ src/local_deploy.sh
@@ -0,0 +1,21 @@
+#!usr/bin/bash
+
+# specifying date
+dt=$(date '+%Y-%m-%d--%H:%M:%S')
+
+# paths
+#which papermill to use (venv)
+path_papermill="/home/humberto/.pyenv/versions/3.8.0/envs/web_scraping/bin/papermill"
+
+# lastest file to run
+file_to_execute="hm-web-scraping-v03.ipynb"
+
+# printing msg
+# echo -e "Starting Web Scraping"
+
+# paths
+path_file="/home/humberto/DS/hm/src/$file_to_execute"
+path_log="/home/humberto/DS/hm/src/logs/$file_to_execute-date:$dt.ipynb"
+
+# runing papermill deploy file on log path
+$path_papermill $path_file $path_log
