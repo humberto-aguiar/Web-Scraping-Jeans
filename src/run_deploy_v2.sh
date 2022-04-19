8bc68479c847c3c1f5fa36aaf0ca9d773120188e
humberto-aguiar <hbasf13@gmail.com>
2022-04-19 15:56:02 -0300

    add deploy v2.sh

---
+++ src/run_deploy_v2.sh
@@ -0,0 +1,21 @@
+#!usr/bin/bash
+
+# specifying date
+dt=$(date '+%Y-%m-%d--%H:%M:%S')
+
+# paths
+#which papermill to use (venv)
+path_papermill="/home/ubuntu/.pyenv/versions/3.8.0/envs/web_scraping/bin/papermill"
+
+# lastest file to run
+file_to_execute="hm-web-scraping-v04.ipynb"
+
+# printing msg
+echo -e "Starting Web Scraping\n"
+
+# paths
+path_file="/home/ubuntu/project/Web-Scraping-Jeans/src/$file_to_execute"
+path_log="/home/ubuntu/project/Web-Scraping-Jeans/src/logs/$file_to_execute-date:$dt.ipynb"
+
+# runing papermill deploy file on log path
+$path_papermill $path_file $path_log
