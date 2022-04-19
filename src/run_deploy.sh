da112e0286f515ae23294bb75a315a9700f2e410
humberto-aguiar <hbasf13@gmail.com>
2022-04-19 12:53:49 -0300

    'add papermill run deploy file.sh'

---
+++ src/run_deploy.sh
@@ -0,0 +1,15 @@
+#!usr/bin/bash
+
+# specifying date
+dt=$(date '+%Y-%m-%d--%H:%M:%S')
+
+# paths
+#which papermill to use (venv)
+path_papermill="/home/ubuntu/.pyenv/versions/3.8.0/envs/web_scraping/bin/papermill"
+
+# lastest file to run
+file_to_execute="hm-web-scraping-v03.ipynb"
+
+# paths
+path_file="/home/ubuntu/project/Web-Scraping-Jeans/src/$file_to_execute"
+path_log="/home/ubuntu/project/Web-Scraping-Jeans/src/logs/$file_to_execute-date:$dt.ipynb
