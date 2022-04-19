7894189493dcf76ba662f2c22d44cbd4bd30e1e0
humberto-aguiar <hbasf13@gmail.com>
2022-04-19 12:57:43 -0300

    add print msg

--- src/run_deploy.sh
+++ src/run_deploy.sh
@@ -10,6 +10,9 @@ path_papermill="/home/ubuntu/.pyenv/versions/3.8.0/envs/web_scraping/bin/papermi
 # lastest file to run
 file_to_execute="hm-web-scraping-v03.ipynb"
 
+# printing msg
+echo -e "Starting Web Scraping"
+
 # paths
 path_file="/home/ubuntu/project/Web-Scraping-Jeans/src/$file_to_execute"
-path_log="/home/ubuntu/project/Web-Scraping-Jeans/src/logs/$file_to_execute-date:$dt.ipynb
+path_log="/home/ubuntu/project/Web-Scraping-Jeans/src/logs/$file_to_execute-date:$dt.ipynb"
